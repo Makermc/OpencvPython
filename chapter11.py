bdf914279508bba154b7f3ad47e50a4e8008c9a8
Maker <m18852703660@gmail.com>
2020-06-02 21:29:23 +0800

    LEARN OPENCV in 3 HOURS with Python https://www.youtube.com/watch?v=WQeoO7MI0Bs

---
+++ chapter11.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+#@TIME  :2020/5/27 23:28
+#@Author:Michael.ma
+
+# DOCUMENT SCANNER
+
---
+++ chapter12.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+#@TIME  :2020/5/27 23:28
+#@Author:Michael.ma
+
+# NUMBER PLATE DETECTION
